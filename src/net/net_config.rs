d3bca26ed8159e5131accca20d1f27cac9f3d15b
fjhg19930505 <ziyoulu1993@163.com>
2020-08-01 17:26:01 +0800

    bug修复

--- src/net/net_config.rs
+++ src/net/net_config.rs
@@ -7,7 +7,7 @@ impl NetConfig {
     pub fn instance() -> &'static mut Config {
         unsafe {
             if el == 0 as *mut _ {
-                el = Box::into_raw(Box::new(C))
+                el = Box::into_raw(Box::new(Config::new_empty()))
             }
 
             &mut *el
@@ -15,8 +15,8 @@ impl NetConfig {
     }
 
     pub fn change_instance(field: &str, proto: &str) -> bool {
-        let config = match Config::new(field, proto).unwrap() {
-            Ok(c) => c,
+        let config = match Config::new(field, proto) {
+            Some(c) => c,
             _ => {
                 return false;
             }
@@ -34,7 +34,8 @@ impl NetConfig {
 
     pub fn change_by_file(file_name: &str) -> bool {
         if let Ok(file_data) = FileUtils::get_file_data(file_name) {
-            let file_data = match String::from_utf8(file_data).ok() {
+
+            let file_data = match String::from_utf8(file_data) {
                 Ok(f) => f,
                 _ => {
                     return false;
@@ -42,7 +43,7 @@ impl NetConfig {
             };
 
             let config = match Config::new_by_full_str(&*file_data) {
-                Ok(c) => c,
+                Some(c) => c,
                 _ => {
                     return false;
                 }
@@ -53,7 +54,7 @@ impl NetConfig {
                     let old = Box::from_raw(el);
                     drop(old);
                 }
-                el = Box::into_raw(Box:new(config));
+                el = Box::into_raw(Box::new(config));
             }
             return true;
         }
